6fab1d1ef608fb7f64ed88a078bc21165ee96274
Cameron Campbell <cameron.campbell325@gmail.com>
2026-01-25 04:45:13 +0000

    removed provider trait

--- crates/anyml_core/src/providers/mod.rs
+++ crates/anyml_core/src/providers/mod.rs
@@ -3,5 +3,3 @@ pub mod list_models;
 
 pub use chat::{ChatChunk, ChatError, ChatOptions, ChatProvider, ChatResponse, ChatStreamError};
 pub use list_models::{ListModelsError, ListModelsProvider};
-
-impl<T: ChatProvider + ListModelsProvider> ProviderTrait for T {}
